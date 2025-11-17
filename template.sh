b7f818d7faf0a8d1c8dc8508c5d465579a83f1ea
Balaji Chinthagunta <balajichinthagunta@Mac.lan>
2025-11-17 18:36:34 -0500

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#creating directory
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/--init--.py
+touch src/helper.py 
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+
+echo "Directory and files are creates succesfully!."
