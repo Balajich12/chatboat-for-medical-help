88310eb5be063396d081180d142c99b6823293c3
Balaji Chinthagunta <balajichinthagunta@Mac.lan>
2025-11-21 12:10:02 -0500

    upadated readme,setup and requiremnts.txt

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # chatboat-for-medicla-help
+```bash
+conda create -n mediboat python=3.10 -y
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name= "medical_chatboat",
+    version= "1.0",
+    author= "Balaji",
+    auythor_email= "chintaguntabalaji@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+
+)
